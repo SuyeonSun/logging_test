ff3892a9b6126cced61c88e6a7e5245b3bf6595b
SuyeonSun <suninisy@gmail.com>
2023-12-26 14:00:14 +0900

    after logger

--- src/main/java/com/example/loggerTest/LoggerTestApplication.java
+++ src/main/java/com/example/loggerTest/LoggerTestApplication.java
@@ -1,12 +1,18 @@
 package com.example.loggerTest;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class LoggerTestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LoggerTestApplication.class, args);
-		System.out.println("====== logger test =====");
+		System.out.println("====== logging test =====");
+		String message = "logging test with logger";
+		log.error("error mode, {}", message);
+		log.info("error mode, {}", message);
+		System.out.println("====== end =====");
 	}
 }
